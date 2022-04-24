123f5c34fc849a9535384052e40b09b89008eae4
Márcio Aquilles <marcioaquilles@yahoo.com.br>
2022-04-24 16:45:31 -0300

    Projeto Easter

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,62 +1,72 @@
 jQuery(document).ready(function ($) {
-  //FIXED HEADER
-  window.onscroll = function () {
-    if (window.pageYOffset > 140) {
-      $("#header").addClass("active");
-    } else {
-      $("#header").removeClass("active");
-    }
-  };
+    //FIXED HEADER
+    window.onscroll = function () {
+        if (window.pageYOffset > 140) {
+            $("#header").addClass("active");
+        } else {
+            $("#header").removeClass("active");
+        }
+    };
 
-  //ISOTOPE
-  let btns = $("#servicos .button-group button");
+    //ISOTOPE
+    let btns = $("#servicos .button-group button");
 
-  btns.click(function (e) {
-    $("#servicos .button-group button").removeClass("active");
-    e.target.classList.add("active");
+    btns.click(function (e) {
+        $("#servicos .button-group button").removeClass("active");
+        e.target.classList.add("active");
 
-    let selector = $(e.target).attr("data-filter");
-    $("#servicos .grid").isotope({
-      filter: selector,
+        let selector = $(e.target).attr("data-filter");
+        $("#servicos .grid").isotope({
+            filter: selector,
+        });
     });
-  });
 
-  $(window).on("load", function () {
-    $("#servicos .grid").isotope({
-      filter: "*",
+    $(window).on("load", function () {
+        $("#servicos .grid").isotope({
+            filter: "*",
+        });
     });
-  });
 
-  //MAGNIFY
-  $(".grid .popup-link").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true,
-      tPrev: "Anterior",
-      tNext: "Próxima",
-      tCounter: "%curr% de %total%",
-    },
-  });
+    //MAGNIFY
+    $(".grid .popup-link").magnificPopup({
+        type: "image",
+        gallery: {
+            enabled: true,
+            tPrev: "Anterior",
+            tNext: "Próxima",
+            tCounter: "%curr% de %total%",
+        },
+    });
 
-  //OWL
-  $(".owl-carousel").owlCarousel({
-    loop: false,
-    margin: 30,
-    autoplay: true,
-    autoplayTimeout: 6000,
-    dots: true,
-    lazyLoad: true,
-    nav: false,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      600: {
-        items: 1,
-      },
-      1000: {
-        items: 2,
-      },
-    },
-  });
+    //OWL
+    $(".owl-carousel").owlCarousel({
+        loop: false,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 6000,
+        dots: true,
+        lazyLoad: true,
+        nav: false,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 2,
+            },
+        },
+    });
 });
+
+var inputAdd = +document.getElementById("more").value;
+var inputLess = +document.getElementById("less").value;
+var outputChoices = document.getElementById("number");
+
+function showNumber() {
+    outputChoices.value += inputAdd.value;
+}
+
+inputAdd.oninput = showNumber;
